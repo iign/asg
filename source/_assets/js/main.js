ba8f434ca5721a5b8a813c3fd709f4021bc3b16a
Ignacio Toledo <ignacio@ign.uy>
2018-10-18 14:45:58 -0300

    Add author info.

--- source/_assets/js/main.js
+++ source/_assets/js/main.js
@@ -1,5 +1,8 @@
-console.log('Main!')
+//console.log('%cBuilt by %cm • n t a g', 'font-size: 12px', 'font-family: Poppins, sans-serif; font-size: 16px; font-weight: bold; color: #FF4455; padding: 2px 6px; border-radius: 2px')
+//console.log('🔗 https://montag.uy')
 window.$ = window.jQuery = require('jquery')
+
+console.log('%c💡 Crafted with ♥️ by https://ign.uy', 'font-size: 14px; padding: 8px 0; font-family: Poppins, sans-serif;')
 
 $('.js-toggle-menu').on('click', function () {
   $('.header__menu').toggleClass('active')
