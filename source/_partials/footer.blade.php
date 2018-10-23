2234e904f84fc502930793d125a79e397b84c13f
Ignacio Toledo <ignacio@ign.uy>
2018-10-23 12:10:33 -0300

    Add footer draft

--- source/_partials/footer.blade.php
+++ source/_partials/footer.blade.php
@@ -1,5 +1,24 @@
 <footer class="footer band">
   <div class="container">
-    COPYRIGHT © ARCHSOURCING 2018
+    <div class="row">
+    <div class="col">
+      <h5 class="footer__title">Contact us</h5>
+    </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="icon-house-wrap">
+          <img class="icon-house" src="assets/images/icon-house.svg" alt="Headquarters">
+        </div>
+        <p>
+          Sinergia Design / Victor Haedo 2240 - Of.09 <br>
+          Montevideo, Uruguay <br>
+        </p>
+        <p>
+          +1 310 598 2214 <br>
+          hello@archsourcing.com
+        </p>
+      </div>
+    </div>
   </div>
 </footer>
