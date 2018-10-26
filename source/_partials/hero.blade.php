50f7b9ccee14293054df9d5c014a58d1e540fca9
Ignacio Toledo <ignacio@ign.uy>
2018-10-26 11:54:30 -0300

    Hero image animation

--- source/_partials/hero.blade.php
+++ source/_partials/hero.blade.php
@@ -1,3 +1,4 @@
 <div class="hero">
-  <img src="assets/images/hero.svg" alt="Your projects developed. You unplug.">
+  {{-- <img src="assets/images/hero.svg" alt="Your projects developed. You unplug."> --}}
+  @include('_partials/hero_image')
 </div>
