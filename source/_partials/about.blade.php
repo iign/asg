bec934d3b4e45b21ecf210d3b375aac56a06af2b
Ignacio Toledo <ignacio@ign.uy>
2018-12-17 09:45:07 -0300

    Update About section

--- source/_partials/about.blade.php
+++ source/_partials/about.blade.php
@@ -14,30 +14,32 @@
       <div class="col-md-5 story">
         <h4 class="service__title">Our Story</h4>
         <p>
-          We could start by once upon a time, but truth is life in architecture is far from being a fairy tale.
+          Architecture takes you to places you cannot imagine. 
+          It’s the most challenging, rewarding, frustrating, vulnerable, inspiring and 
+          galvanizing profession. 
         </p>
         <p>
-          As architects we know architecture takes you to places you cannot imagine. It’s the most challenging, rewarding, frustrating,
-          vulnerable, inspiring and galvanizing profession.
+          We architects are passionate, and solving problems through the art of 
+          design stirs our soul out. The hardest challenge is to coordinate not 
+          only your projects but your personal life with long hours at the 
+          office to hammer out a great final solution. 
         </p>
         <p>
-          We are passionate and solving problems through the art of design stirs our soul out. The hardest challenge is to coordinate
-          not only your projects but your personal life with long hours at the office to hammer out a great final solution.
+          We believe things can be done quicker, smarter, better.
         </p>
         <p>
-          We believed things could be done quicker, smarter, better.
-        </p>
-        <p>
-          By using the latest tools for BIM technology, ASG helps busy architects to develop their projects and streamline production
-          with pioneer technology while they focus on expanding their company and boost quality time.
+          By using latest BIM technology tools, /ARCHSOURCING helps busy 
+          architects to develop their projects, reduce costs and streamline 
+          production while clients focus on expanding their company and boost quality time. 
         </p>
 
       </div>
       <div class="col-md-5">
         <h4 class="service__title">Our Purpose</h4>
         <p class="purpose">
-          <em>We believe</em> architects can have a more balanced life. Architects are creative, imaginative and ingenious
-          creatures. We believe that this creative thinking power should be aimed to solving complex problems and
+          We believe in <em>technology</em> as a tool to improve our lives. 
+          We believe in <em>creative thinking</em> as the key for successful solutions. 
+          We are <em>leading change in architecture</em>. Join us! 
         </p>
         <img class="green-arrows green-arrows--inverted" src="/assets/images/green-arrows.svg" alt="Arrows">
       </div>
