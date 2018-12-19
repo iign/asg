94d304f1bcb4a9efbad65cd7218f634c77d128d5
Ignacio Toledo <ignacio@ign.uy>
2018-12-19 14:42:57 -0300

    Update site title in configs

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     'baseUrl' => 'http://localhost:3000/',
-    'siteName' => '/ASG - ARCHSOURCING',
+    'siteName' => '/ARCHSOURCING - Your projects developed. You unplug.',
     'siteDescription' => 'We\'re a cutting edge firm that helps busy architects to develop their BIM projects. We completely adapt to your standards and deliver like an in-house team.',
     'production' => false,
     'collections' => [],
