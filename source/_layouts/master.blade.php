01a1899166aa1f566eaa63ba1e11b9cf52eff018
Ignacio Toledo <ignacio@ign.uy>
2021-03-25 14:19:42 -0300

    Add hotjar

--- source/_layouts/master.blade.php
+++ source/_layouts/master.blade.php
@@ -1,13 +1,33 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-184214813-1"></script>
+
+<head>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-184214813-1"></script>
     <script>
         window.dataLayer = window.dataLayer || [];
-        function gtag(){dataLayer.push(arguments);}
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
         gtag('js', new Date());
 
         gtag('config', 'UA-184214813-1');
+    </script>
+    <script>
+        (function(h, o, t, j, a, r) {
+            h.hj = h.hj || function() {
+                (h.hj.q = h.hj.q || []).push(arguments)
+            };
+            h._hjSettings = {
+                hjid: 2316996,
+                hjsv: 6
+            };
+            a = o.getElementsByTagName('head')[0];
+            r = o.createElement('script');
+            r.async = 1;
+            r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+            a.appendChild(r);
+        })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
     </script>
     <title>{{ $page->siteName }}</title>
     <meta name="description" content="{{ $page->siteDescription }}">
@@ -32,10 +52,11 @@
     <meta property="og:url" content="{{ $page->getUrl() }}" />
     <meta property="og:image" content="{{ $page->getUrl() }}asg.jpg" />
     <meta property="og:description" content="{{ $page->siteDescription }}" />
-    
-  </head>
-  <body id="up">
-       
+
+</head>
+
+<body id="up">
+
     @include('_partials/header')
     @include('_partials/covid')
     @include('_partials/hero')
@@ -57,14 +78,13 @@
 
     @include('_partials/footer')
 
-    <script src="//code.jquery.com/jquery-3.3.1.slim.min.js"
-            integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E="
-            crossorigin="anonymous"></script>
+    <script src="//code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha256-3edrmyuQ0w65f8gfBsqowzjJe2iM6n0nKciPUp8y+7E=" crossorigin="anonymous"></script>
     <script src='//api.mapbox.com/mapbox-gl-js/v0.50.0/mapbox-gl.js'></script>
     <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.6/ScrollMagic.min.js"></script>
     <script src="//cdn.jsdelivr.net/gh/cferdinandi/smooth-scroll@14/dist/smooth-scroll.polyfills.min.js"></script>
     <script src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
 
     <script src="{{ mix('js/main.js', 'assets/build') }}"></script>
-  </body>
+</body>
+
 </html>
